405647ed2298bcdc74e282d1742ca9968a76645a
NguyenDuongQuang <103760903+NguyenDuongQuang@users.noreply.github.com>
2023-11-01 14:14:03 +0700

    fix
    fix

--- fontend/src/constants/url.js
+++
@@ -1,5 +0,0 @@
-export const HOST =
-  process.env.REACT_APP_API_URL || "http://localhost:8080/api";
-// export const HOST = process.env.REACT_APP_API_URL || "http://14.225.204.147:8080/api"
-
-export const IMAGE_HOST = HOST + "upload/";
